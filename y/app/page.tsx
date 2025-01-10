7b7ea4ef2bf527860d0f7240ef590c3a6d06268b
Olzhas Yergali <olzhasergali56@gmail.com>
2025-01-10 23:44:22 +0500

    finish the filters and starting to get the data about products

--- y/app/page.tsx
+++ y/app/page.tsx
@@ -4,59 +4,20 @@ import ProductCard from "@/components/shared/product-card";
 import ProductsGroupList from "@/components/shared/products-group-list";
 import { Title } from "@/components/shared/title";
 import TopBar from "@/components/shared/top-bar";
+import { prisma } from "@/prisma/prisma-client";
 
-export default function Home() {
-  const products = [
-    {
-      id: 0,
-      name: "Креветки со сладким чили",
-      price: 550,
-      items: [{price: 550}],
-      imageUrl: "https://media.dodostatic.net/image/r:233x233/11ef01fee6dd7261a9c36187149758d0.avif",
-    },
-    {
-      id: 1,
-      name: "Пепперони",
-      price: 450,
-      items: [{price: 550}],
-      imageUrl: "https://media.dodostatic.net/image/r:233x233/11ef01fee6dd7261a9c36187149758d0.avif",
-    },
-    {
-      id: 2,
-      name: "Маргарита",
-      price: 400,
-      items: [{price: 550}],
-      imageUrl: "https://media.dodostatic.net/image/r:233x233/11ee7d5f837255b58b25a62c60ffdb38.avif",
-    },
-    {
-      id: 3,
-      name: "Четыре сыра",
-      price: 500,
-      items: [{price: 550}],
-      imageUrl: "https://media.dodostatic.net/image/r:233x233/11ee7d5f837255b58b25a62c60ffdb38.avif",
-    },
-    {
-      id: 4,
-      name: "Цезарь",
-      price: 600,
-      items: [{price: 550}],
-      imageUrl: "https://media.dodostatic.net/image/r:233x233/11ee7d5f837255b58b25a62c60ffdb38.avif",
-    },
-    {
-      id: 5,
-      name: "Ветчина и грибы",
-      price: 480,
-      items: [{price: 550}],
-      imageUrl: "https://media.dodostatic.net/image/r:233x233/11ee7d5f837255b58b25a62c60ffdb38.avif",
-    },
-    {
-      id: 6,
-      name: "Ветчина и грибы",
-      price: 480,
-      items: [{price: 550}],
-      imageUrl: "https://media.dodostatic.net/image/r:233x233/11ee7d5f837255b58b25a62c60ffdb38.avif",
-    },
-  ];
+export default async function Home() {
+
+  const categories = await prisma.category.findMany({
+    include:{
+      products:{
+        include:{
+          ingredients: true,
+          variance: true
+        }
+      }
+    }
+  });
   
   return (
     <>
@@ -71,9 +32,20 @@ export default function Home() {
             </div>
             <div className="flex-1">
               <div className="flex flex-col gap-16">
-                <ProductsGroupList title="Пиццы" items = {products} categoryId={1}/>
-                <ProductsGroupList title="Комбо" items = {products} categoryId={2}/>
-              </div>
+                {categories.map(category => 
+                  category.products.length > 0 && (
+                    <ProductsGroupList
+                      key={category.id}
+                      title={category.name}
+                      categoryId={category.id}
+                      items = {
+                        category.products
+                      }
+
+                      />
+                  )
+                )}
+             </div>
             </div>
         </div>
       </Container>
